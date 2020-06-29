215986f7599ed152e8078d7a1e9053eccde390c5
Jesse Dyer <jesse@jesses-air.lan>
2020-06-28 22:55:33 -0700

    App start

---
+++ test/test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Jesse Dyer on 6/28/20.
+//  Copyright © 2020 Gingko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Jesse!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
