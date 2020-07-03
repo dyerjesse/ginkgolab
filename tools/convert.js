e948d7fe5c8d1bceb87fc5738cde7ba371b52bea
Jeff Dyer <jeffdyer@acm.org>
2020-07-03 12:56:05 -0700

    Add converter

---
+++ tools/convert.js
@@ -0,0 +1,76 @@
+const https = require('https');
+const http = require('http');
+const csv = require('csv-parser');
+const fs = require('fs');
+
+function rmdir(path) {
+  try { var files = fs.readdirSync(path); }
+  catch(e) { return; }
+  if (files.length > 0) {
+    for (var i = 0; i < files.length; i++) {
+      var filePath = path + '/' + files[i];
+      if (fs.statSync(filePath).isFile()) {
+        fs.unlinkSync(filePath);
+      } else {
+	rmdir(filePath);
+      }
+    }
+  }
+  fs.rmdirSync(path);
+}
+
+function mkdir(path) {
+  fs.mkdirSync(path);
+}
+
+function cldir(path) {
+  rmdir(path);
+  mkdir(path);
+}
+
+function exec(cmd, args) {
+  console.log("exec() cmd=" + cmd);
+  execSync(cmd, args);
+}
+
+function clean() {
+  console.log("Cleaning...");
+  cldir('./build');
+  mkdir('./build/data');
+}
+
+function build() {
+  clean();
+  convert();
+}
+
+const FILE_IN = './app/convert.csv';
+const FILE_OUT = './data/convert.json';
+
+function loadData(filename, resume) {
+  const data = [];
+  fs.createReadStream(filename)
+    .pipe(csv())
+    .on('data', (row) => {
+      data.push(row);
+    })
+    .on('end', () => {
+      resume([], data);
+    });
+}
+
+function convert() {
+  const data = [];
+  loadData(FILE_IN, (err, rawData) => {
+    rawData.forEach((row, i) => {
+      console.log("convert row=" + JSON.stringify(row, null, 2));
+      data.push(row);
+    });
+    fs.writeFile(FILE_OUT, JSON.stringify(data, null, 2), () => {
+      console.log(data.length + ' data written to ' + FILE_OUT);
+    });
+  });
+}
+
+convert();
+
